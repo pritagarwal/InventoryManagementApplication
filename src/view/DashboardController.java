99e1350f1b60c31819f29833dde7530c9699cba9
PritAgarwal <106642453+pritagarwal@users.noreply.github.com>
2023-10-26 01:32:52 +0530

    Update DashboardController.java

--- src/view/DashboardController.java
+++ src/view/DashboardController.java
@@ -8,16 +8,10 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
 
-/**
- * FXML Controller class
- *
- * @author Manoj
- */
+
 public class DashboardController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+ 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
